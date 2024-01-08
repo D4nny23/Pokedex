79c640a7d3ae36c13180637cd02491fbde624d7c
Daniel Stanciu <marti90@hotmail.es>
2024-01-08 10:05:31 +0100

    First Commit

---
+++ main.js
@@ -0,0 +1,146 @@
+inicio();
+
+function inicio() {
+  let divContenedor = document.getElementById("contenedor");
+  for (let i = 1; i < 1018; i++) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://pokeapi.co/api/v2/pokemon/" + i);
+
+    xhr.responseType = "json";
+    let div = document.createElement("div");
+    div.setAttribute("class", "celda");
+    divContenedor.appendChild(div);
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        let name = document.createElement("p");
+        name.textContent = xhr.response.name.toUpperCase();
+        div.appendChild(name);
+        recuperoImagen(xhr, div, "imgLista");
+      } else if (xhr.status === 404) {
+        Swal.fire({
+          title: "No existe el Pokemon",
+          icon: "error",
+          confirmButtonColor: "#ff0000",
+        });
+      }
+    };
+    xhr.onerror = function () {
+      document.getElementById("resultado").textContent = "Error";
+    };
+
+    xhr.send();
+  }
+}
+
+function recuperoImagen(xhr, div, id) {
+  let img = xhr.response.sprites.front_default;
+  let icon = document.createElement("img");
+  icon.setAttribute("src", img);
+  icon.setAttribute("id", id);
+  div.appendChild(icon);
+}
+
+// -------------------------------------------------------------------------------------- Para el detalle del Pokemon
+
+document.getElementById("buscaPokemon").addEventListener("click", () => {
+  let xhr = new XMLHttpRequest();
+  let nombrePokemon =
+    document.getElementById("pokemonName").value != ""
+      ? document.getElementById("pokemonName").value
+      : 0;
+  let ruta = "https://pokeapi.co/api/v2/pokemon/" + nombrePokemon;
+  xhr.open("GET", ruta);
+  console.log(ruta);
+
+  xhr.responseType = "json";
+  xhr.onload = function () {
+    let body = document.getElementsByTagName("body")[0];
+
+    recuperarEstadisticas(xhr);
+    if (!document.getElementById("divPokemon")) {
+      let div = document.createElement("div");
+      div.setAttribute("id", "divPokemon");
+      body.appendChild(div);
+    }
+
+    if (xhr.status === 200) {
+      detallesPokemon(xhr);
+      detallesBasicos(xhr);
+    } else if (xhr.status === 404) {
+      status404(body);
+    }
+  };
+
+  xhr.onerror = function () {
+    div.textContent = "Error";
+  };
+
+  xhr.send();
+});
+
+function status404(body) {
+  if (!document.getElementById("contenedor")) {
+    let div = document.createElement("div");
+    div.setAttribute("id", "contenedor");
+    body.appendChild(div);
+  }
+  inicio();
+  if (document.getElementById("divPokemon")) {
+    document.getElementById("divPokemon").remove();
+  }
+  Swal.fire({
+    title: "No existe el Pokemon",
+    icon: "error",
+    confirmButtonColor: "#ff0000",
+  });
+}
+
+function detallesPokemon(xhr) {
+  let div = document.getElementById("divPokemon");
+  recuperoImagen2(xhr, div, "imgDetalle");
+  //Borra la lista de Pokemon
+  if (document.getElementById("contenedor")) {
+    let divContenedor = document.getElementById("contenedor");
+    divContenedor.remove();
+  }
+}
+
+function recuperoImagen2(xhr, div, id) {
+  let img = xhr.response.sprites.front_default;
+  //Si no existe el elemento IMG lo creo y le meto la imagen del pokemon
+  if (!document.getElementById(id)) {
+    let icon = document.createElement("img");
+    icon.setAttribute("src", img);
+    icon.setAttribute("id", id);
+    div.appendChild(icon);
+  } else {
+    //Si existe el elemento lo recupero y le meto la imagen nueva
+    let imgElement = document.getElementById(id);
+    imgElement.setAttribute("src", img);
+  }
+}
+
+//PRUEBA
+function recuperarEstadisticas(xhr) {
+  // let stats= xhr.response.stats;
+  // for (let i = 0; i < stats.length; i++) {
+  //   console.log("Estadística básica"+stats[i].stat.name);
+  // }
+}
+
+//Recupero detalles básicos como nombre, id, peso...
+function detallesBasicos(xhr) {
+  if (!document.getElementById("detallesBásicos")) {
+    let divDetallesBasicos = document.createElement("div");
+    divDetallesBasicos.setAttribute("id", "detallesBasicos");
+    let detalles = 'Id: ,Name: ,Base experience: ,Height: ,Default: ,Order: ,Weight: '.split(",");
+    let detallesPeticion = [xhr.response.id,xhr.response.name,xhr.response.base_experience,xhr.response.height,xhr.response.is_default,xhr.response.order,xhr.response.weight];
+    for (let i = 0; i < detalles.length; i++) {
+      let detalle = document.createElement("p");
+      detalle.setAttribute("class", "atributosBasicos");
+      detalle.textContent= (detalles[i]+ detallesPeticion[i]).toUpperCase();
+      divDetallesBasicos.appendChild(detalle);
+    }
+    document.getElementById("divPokemon").appendChild(divDetallesBasicos);
+  }
+}
